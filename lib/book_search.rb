91b185a180ff35dadc8cb408c4d8eba475b30c23
ted_scanlan <t.scanlan92@gmail.com>
2019-11-04 15:10:52 +0000

    navigate added

--- lib/book_search.rb
+++ lib/book_search.rb
@@ -2,10 +2,49 @@
 
 class BookSearch
 
-def get_title
 
-$stdin.gets.chomp
+  def initialize
+
+  @title
+  @results =[]    #make results own class
 
 end
+
+def get_title
+
+  welcome_message
+  input = $stdin.gets.chomp
+
+  $stdin.gets.chomp
+
+end
+
+
+
+def welcome_message
+  puts `clear`
+  puts ""
+  puts ""
+  print "Please enter the title you'd like to search (or RL to view your reading list): "
+  puts ""
+  puts ""
+
+
+end
+
+def navigate
+  input = $stdin.gets.chomp
+  if input == 'R'
+    start
+  elsif input == 'RL'
+    show_list
+  elsif input == 'exit'
+    Kernel.exit(true)
+  else
+    print "incorrect input"
+  end
+end
+
+
 
 end
